e5f5cdec5405dc0bf08fb2f5f744bf34440d1e58
Matthias Felleisen <matthias@ccs.neu.edu>
2015-08-20 17:46:04 -0400

    Racket-y style

--- anagram/example.rkt
+++ anagram/example.rkt
@@ -1,15 +1,20 @@
 #lang racket
 
-(provide anagrams-for)
+(provide
+ ;; String [Listof String] -> [Listof String]
+ ;; pick candidates for anagrams from the given list
+ ;; def. a candidate consists of the same letters as subject
+ anagrams-for)
 
+;; -----------------------------------------------------------------------------
 (define (anagrams-for subject candidates)
   (filter (lambda (w) (anagram? w subject)) candidates))
 
+;; String String -> Boolean
+;; is a an anagram for b?
 (define (anagram? a b)
-  (let ([sorted-string
-         (lambda (s)
-           (apply string (sort (string->list s) char-ci<?)))])
-    (and
-     (string-ci=? (sorted-string a)
-                  (sorted-string b))
-     (not (string-ci=? a b)))))
+  (define (sorted-string s)
+    (list->string (sort (string->list s) char-ci<?)))
+  (and
+   (string-ci=? (sorted-string a) (sorted-string b))
+   (not (string-ci=? a b))))
