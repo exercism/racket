72ea982ab688ecd9ee0701128333a70ab6526634
Floyd Arguello <floyd.arguello@gmail.com>
2015-08-20 10:22:42 -0500

    follow racket style guide

--- word-count/example.rkt
+++ word-count/example.rkt
@@ -1,8 +1,8 @@
 #lang racket
 
+(provide word-count)
+
 (define (word-count word)
   (for/fold ([result (hash)])
             ([word (in-list (string-split word))])
             (hash-set result word (add1 (hash-ref result word 0)))))
-
-(provide word-count)
