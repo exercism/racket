e18ecf2b9221f8ca61594278ad9828a14e0a304e
Floyd Arguello <floyd.arguello@gmail.com>
2015-12-10 12:15:10 -0600

    run tests in parallel, more succinct output

--- bin/check-exercises.sh
+++ bin/check-exercises.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env sh
 
+set -e
+
 for exercise in *; do
     if [ -d "$exercise" ]; then
-        cd "$exercise" || exit
-        if [ -f "$exercise"-test.rkt ]; then
-            echo Testing "$exercise"...
-            if [ -f "$exercise".rkt ]; then
-                mv "$exercise".rkt "$exercise".rkt_
+        if [ -f "$exercise/$exercise"-test.rkt ]; then
+            if [ -f "$exercise/$exercise".rkt ]; then
+                mv "$exercise/$exercise".rkt "$exercise/$exercise".rkt_
             fi
-            mv example.rkt "$exercise".rkt
-            raco test "$exercise"-test.rkt
-            SUCCESS=$?
-            mv "$exercise".rkt example.rkt
-            if [ -f "$exercise".rkt_ ]; then
-                mv "$exercise".rkt_ "$exercise".rkt
-            fi
-            if [ $SUCCESS -ne 0 ]; then
-                exit $SUCCESS
-            fi
+            mv "$exercise"/example.rkt "$exercise/$exercise".rkt
         fi
-        cd ..
     fi
 done
+raco test -x --jobs 5 -Q --table .
+SUCCESS=$?
+for exercise in *; do
+    if [ -f "$exercise/$exercise".rkt_ ]; then
+        mv "$exercise/$exercise".rkt "$exercise"/example.rkt
+        mv "$exercise/$exercise".rkt_ "$exercise/$exercise".rkt
+    fi
+done
+if [ $SUCCESS -ne 0 ]; then
+    exit $SUCCESS
+fi
