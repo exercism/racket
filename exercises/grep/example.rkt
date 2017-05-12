d8f8f659b21a649b021a0b3ff2c54171b49126b4
Andrei Formiga <archimedes_siracusa@hotmail.com>
2017-05-12 17:20:41 -0300

    Reimplement grep and its tests to conform to specifications

--- exercises/grep/example.rkt
+++ exercises/grep/example.rkt
@@ -1,28 +1,62 @@
 #lang racket
 
-(provide grep-file)
+(provide grep)
 
-(define (grep-file f pattern)
-  (with-input-from-file f
-    (lambda ()
-      (for/list ([line (in-lines)] ;; Iterate over lines in file, lazily
-                 [line-number (in-naturals 1)] ;; Iterate over natural numbers, lazily
-                 #:when (regexp-match? pattern line))
-        (cons line-number line)))))
+(struct flags (lineno case-sensitive filename whole invert))
 
-;; =============================================================================
+(define (member? v lst)
+  (if (member v lst) #t #f))
 
-;; Code inside the "main" module is run in Dr.Racket or on the command-line,
-;;  but not when the file is included as a library.
-(module+ main
-  (require racket/cmdline)
-  (command-line
-   #:program "racket-grep"
-   #:args (pat-str . file*)
-   (begin
-     (define pattern (regexp pat-str))
-     (for ([f (in-list file*)])
-       (for ([num+str (in-list (grep-file f pattern))])
-         (printf "~a\n~a\n" f (make-string (string-length f) #\-))
-         (printf "~a : ~a\n" (car num+str) (cdr num+str))
-         (newline))))))
+(define (process-flags fs)
+  (flags
+   (member? "-n" fs)
+   (not (member? "-i" fs))
+   (member? "-l" fs)
+   (member? "-x" fs)
+   (member? "-v" fs)))
+
+(define (add-line-numbers lines)
+  (for/list ([n (in-naturals 1)]
+             [line (in-list lines)])
+    (cons line n)))
+
+(define (process-pattern pat flags)
+  (define cpat (regexp-quote pat (flags-case-sensitive flags)))
+  (if (flags-whole flags)
+      (string-append "^" cpat "$")
+      cpat))
+
+(define (grep-file f pat flags mod print-filename?)
+  (define lines (add-line-numbers (file->lines f)))
+  (define line-filter
+    (lambda (line) (mod (regexp-match? pat (car line)))))
+  (define res (filter line-filter lines))
+  (define (add-lines matches)
+    (if (flags-lineno flags)
+        (map (lambda (m) (format "~a:~a" (cdr m) (car m))) matches)
+        (map (lambda (m) (car m)) matches)))
+  (define (add-filenames result)
+    (if print-filename?
+        (map (lambda (s) (format "~a:~a" f s)) result)
+        result))
+  (add-filenames (add-lines res)))
+
+(define (file-contains? f pat)
+  (regexp-match? pat (file->string f)))
+
+(define (grep fls pat files)
+  (define flags (process-flags fls))
+  (define repat (process-pattern pat flags))
+  (define filter-mod
+    (if (flags-invert flags)
+        not
+        identity))
+  (define file-filter
+    (lambda (f) (filter-mod (file-contains? f repat))))
+  (define print-filename? (> (length files) 1))
+  (if (flags-filename flags)
+      (filter file-filter files)
+      (apply append
+             (map (lambda (f)
+                    (grep-file f repat flags filter-mod print-filename?))
+                  files))))
--- exercises/grep/grep.rkt
+++ exercises/grep/grep.rkt
@@ -1,20 +1,3 @@
 #lang racket
 
-(provide
-  grep-file
-  ;; (-> Path-String Regexp (Listof (Pairof Natural String)))
-  ;; Find lines in a file matching a regular expression pattern.
-  ;; Output is a list of pairs containing the number and contents of
-  ;;  each matching line.
-)
-
-
-;; =============================================================================
-
-(module+ main
-  ;; Extra challenge: fill in this submodule so that running
-  ;;  `racket grep.rkt PATTERN FILE ...`
-  ;; runs grep on each FILE searching for PATTERN, and prints the results
-  ;; to stdout.
-  (printf "Not implemented!\n")
-)
+(provide grep)
