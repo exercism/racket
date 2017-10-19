9bbf043d637ef69237cb0727d4a23a292bccb9a1
jgilray <jgilray@gmail.com>
2017-10-18 19:18:12 -0700

    updated example.rkt and tests

--- exercises/collatz-conjecture/collatz-conjecture-test.rkt
+++ exercises/collatz-conjecture/collatz-conjecture-test.rkt
@@ -9,12 +9,26 @@
     (test-suite
      "collatz-conjecture tests"
 
-     (test-eqv? "zero steps for one" (collatz 1) 0)
-     (test-eqv? "divide if even" (collatz 16) 4)
-     (test-eqv? "even and odd steps" (collatz 12) 9)
-     (test-eqv? "Large number of even and odd steps" (collatz 1000000) 152)
-     (test-eqv? "zero is an error" (collatz 0) "Error: number must be a positive integer")
-     (test-eqv? "negative value is an error" (collatz -15) "Error: number must be a positive integer")
-     (test-eqv? "non exact value is an error" (collatz 3.4) "Error: number must be a positive integer")))
+     (test-eqv? "zero steps for one"
+                (collatz 1)
+                0)
+     (test-eqv? "divide if even"
+                (collatz 16)
+                4)
+     (test-eqv? "even and odd steps"
+                (collatz 12)
+                9)
+     (test-eqv? "Large number of even and odd steps"
+                (collatz 1000000)
+                152)
+     (test-exn "zero is an error"
+               exn:fail?
+               (lambda () (collatz 0)))
+     (test-exn "negative value is an error"
+               exn:fail?
+               (lambda () (collatz -15)))
+     (test-exn "non exact value is an error"
+               exn:fail?
+               (lambda () (collatz 3.4)))))
 
   (run-tests suite))
--- exercises/collatz-conjecture/example.rkt
+++ exercises/collatz-conjecture/example.rkt
@@ -2,22 +2,15 @@
 
 (provide collatz)
 
-; uses a hash table to speed up subsequent calls
-(define (collatz-length ht n)
-  (cond 
-    [(hash-ref ht n #f) => (lambda (colval) colval)]
-    [else
-     (define colval 0)
-     (if (even? n) 
-         (set! colval (add1 (collatz-length ht (/ n 2))))
-         (set! colval (add1 (collatz-length ht (add1 (* 3 n))))))
-     (hash-set! ht n colval)
-     colval]))
-
 (define (collatz n)
-  (define h (make-hash (list '(1 . 0))))
-  (if (and (exact-integer? n) (positive? n))
-      (collatz-length h n)
-      "Error: number must be a positive integer"))
+  (if (= n 1)
+      0
+      (if (and (exact-integer? n) (positive? n))
+          (add1
+           (if (even? n) 
+               (collatz (/ n 2))
+               (collatz (add1 (* 3 n)))))
+          (error "number must be a positive integer"))))
   
 
+  
