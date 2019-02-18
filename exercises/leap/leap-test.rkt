7ef34d655215a8abda2c080b2296e023d77d5a7c
Robert Postill <robert@grinning-cat.com>
2019-02-18 19:09:39 +1100

    removed trailing bracket

--- exercises/leap/leap-test.rkt
+++ exercises/leap/leap-test.rkt
@@ -14,7 +14,6 @@
      (test-eqv? "non-leap-even-year" (leap-year? 1998) #f)
      (test-eqv? "century" (leap-year? 2100)#f)
      (test-eqv? "not an exceptional-century" (leap-year? 1800) #f)
-     (test-eqv? "exceptional-century" (leap-year? 2000) #t))
-    ))
+     (test-eqv? "exceptional-century" (leap-year? 2000) #t)))
 
   (run-tests suite))
