0ec689385cbe8de5b907ceb7570648106746ba4e
Tim Meehan <btmeehan@gmail.com>
2019-05-14 22:11:11 -0700

    Removed the contract from the skeleton input, so that the student can implement it themselves.

--- exercises/affine-cipher/affine-cipher.rkt
+++ exercises/affine-cipher/affine-cipher.rkt
@@ -1,13 +1,4 @@
 #lang racket/base
-(require racket/contract)
-
-(provide (contract-out
-          [encode (string?
-                   exact-nonnegative-integer?
-                   exact-nonnegative-integer? . -> . string?)]
-          [decode (string?
-                   exact-nonnegative-integer?
-                   exact-nonnegative-integer? . -> . string?)]))
 
 (define (encode msg a b)
   "not implemented")
