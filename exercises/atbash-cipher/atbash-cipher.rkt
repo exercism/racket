6793d50ff1f2704be9f773729fc71e06e0c9b0ce
Tim Meehan <btmeehan@gmail.com>
2019-05-15 21:05:43 -0700

    Moved working example to 'example.rkt', created skeleton problem file.

--- exercises/atbash-cipher/atbash-cipher.rkt
+++ exercises/atbash-cipher/atbash-cipher.rkt
@@ -1,34 +1,7 @@
 #lang racket
 
-(provide (contract-out
-          [encode (string? . -> . string?)]
-          [decode (string? . -> . string?)]))
-
-(define (prepare-string m)
-  (filter (λ(s)
-            (or (char-numeric? s)
-                (char-alphabetic? s)))
-          (string->list
-           (string-downcase m))))
-
-(define (atbash-char c)
-  (if (char-numeric? c)
-      c
-      (integer->char
-       (+ (char->integer #\a)
-          (- (char->integer #\z) (char->integer c))))))
-
-(define (group v chunk-size)
-  (let loop ([v v]
-             [N (length v)]
-             [answer '()])
-    (if (<= N chunk-size)
-        (string-join (reverse (cons (list->string v) answer)))
-        (let-values ([(h t) (split-at v chunk-size)])
-          (loop t (- N chunk-size) (cons (list->string h) answer))))))
-
 (define (encode m)
-  (group (map atbash-char (prepare-string m)) 5))
+  (error "not implemented"))
 
 (define (decode m)
-  (list->string (map atbash-char (prepare-string m))))
+  (error "not implemented"))
