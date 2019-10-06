86df7cf869c3e05f6b7d6b12c1aeaad2de18e2ef
Tim Meehan <btmeehan@gmail.com>
2019-10-05 22:32:40 -0700

    Added hint, tinkered with problem ...

--- exercises/robot-name/robot-name-test.rkt
+++ exercises/robot-name/robot-name-test.rkt
@@ -1,4 +1,14 @@
 #lang racket
+(require "robot-name.rkt")
+
+(module+ test
+  (require rackunit
+           rackunit/text-ui)
+  (define suite
+    (test-suite
+     "robot name test"))
+
+  (run-tests suite))
 
 (define (check-robot-name n)
   (regexp-match #px"\\p{Lu}{2}\\p{Nd}{4}" n))
--- exercises/robot-name/robot-name.rkt
+++ exercises/robot-name/robot-name.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require racket/class)
 
 (define (random-digit-char)
   (integer->char
@@ -10,9 +11,30 @@
    (random (char->integer #\A)
            (add1 (char->integer #\Z)))))
 
-(define (generate-robot-name)
-  (list->string
-   (let loop ([index 0])
-     (cond [(< index 2) (cons (random-letter-char) (loop (add1 index)))]
-           [(< index 6) (cons (random-digit-char) (loop (add1 index)))]
-           [else empty]))))
+(define (generate-valid-name)
+  (let ([all-robot-names (set)])
+    (list->string
+     (let loop ([index 0])
+       (cond [(< index 2) (cons (random-letter-char) (loop (add1 index)))]
+             [(< index 6) (cons (random-digit-char) (loop (add1 index)))]
+             [else empty])))))
+
+(define generate-unique-name
+  (let ([all-robot-names (mutable-set)])
+    (lambda ()
+      (let ([name (generate-valid-name)])
+        (cond [(set-member? all-robot-names name)
+               (generate-unique-name)]
+              [else
+               (set-add! all-robot-names name)
+               name])))))
+
+; https://docs.racket-lang.org/guide/classes.html
+(define robot%
+  (class object%
+    (super-new)
+    (define name (generate-unique-name))
+    (define/public (get-name)
+      name)
+    (define/public (reset)
+      (set! name (generate-unique-name)))))
