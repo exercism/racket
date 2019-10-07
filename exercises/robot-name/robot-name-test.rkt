1ecf9053950b5b17562d825b22c3fd77ca03d674
Tim Meehan <btmeehan@gmail.com>
2019-10-06 23:49:10 -0700

    Finished the tests, added 3 unit tests.

--- exercises/robot-name/robot-name-test.rkt
+++ exercises/robot-name/robot-name-test.rkt
@@ -1,14 +1,38 @@
 #lang racket
-(require "robot-name.rkt")
+(require racket/class
+         "robot-name.rkt")
 
 (module+ test
   (require rackunit
            rackunit/text-ui)
-  (define suite
+
+  (define proper-robot-name #px"\\p{Lu}{2}\\p{Nd}{4}")
+  
+  (define (check-proper-robot-name r)
+    (check-regexp-match proper-robot-name (send r get-name)))
+  
+  (define robot-name-suite 
     (test-suite
-     "robot name test"))
+     "Check proper operation of 'robots'"
+     
+     (test-case
+      "Check that robot names conforms to the expected standard."
+      (check-proper-robot-name (new robot%)))
 
-  (run-tests suite))
+     (test-case
+      "Check that resetting a robot name also gives a proper name."
+      (let ([robby (new robot%)])
+        (check-regexp-match proper-robot-name (send robby get-name))
+        (send robby reset)
+        (check-regexp-match proper-robot-name (send robby get-name))))
 
-(define (check-robot-name n)
-  (regexp-match #px"\\p{Lu}{2}\\p{Nd}{4}" n))
+     (test-case
+      "Check that robots are created with unique names."
+      (check-eq?
+       (set-count
+        (list->set
+         (for/list ([i (in-range 10)])
+           (send (new robot%) get-name))))
+       10))))
+
+    (run-tests robot-name-suite))
