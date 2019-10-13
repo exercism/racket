c85ef8809dda0c2478e0707a7a0158fe112767b5
serialhex <serialhex@gmail.com>
2019-10-13 19:28:33 -0400

    finished my example

--- exercises/triangle/example.rkt
+++ exercises/triangle/example.rkt
@@ -2,4 +2,27 @@
 
 (provide triangle?)
 
-(define (triangle? sides kind))
+(define (triangle? sides kind)
+  (let ([a (first sides)]
+        [b (second sides)]
+        [c (third sides)])
+    (if (and (< 0 a) (< 0 b) (< 0 c)
+            (> (+ a b) c)
+            (> (+ a c) b)
+            (> (+ b c) a))
+        (case kind
+          [(equilateral)  (test-equilateral a b c)]
+          [(isosceles)    (test-isosceles a b c)]
+          [(scalene)      (test-scalene a b c)])
+        #f)))
+
+(define (test-equilateral a b c)
+  (= a b c))
+
+(define (test-isosceles a b c)
+  (or (= a b)
+      (= a c)
+      (= b c)))
+
+(define (test-scalene a b c)
+  (not (test-isosceles a b c)))
