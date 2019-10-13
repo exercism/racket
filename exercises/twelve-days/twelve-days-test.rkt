950167e40f9ccdb2716e354df0ccbf22a8b17d7f
serialhex <serialhex@gmail.com>
2019-10-13 16:36:43 -0400

    Should all work now.  Maybe I should add more tests, but I think 3 are fine for now...

--- exercises/twelve-days/twelve-days-test.rkt
+++ exercises/twelve-days/twelve-days-test.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 
+(require "example.rkt")
+
 (module+ test
-  (require "twelve-days.rkt")
 
   (require rackunit rackunit/text-ui)
 
@@ -9,7 +10,7 @@
     (test-suite
       "twelve-days tests"
 
-    (test-eqv? "recites the whole song"
+    (test-equal? "recites the whole song"
       (twelve-days)
       "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.
 
@@ -33,4 +34,20 @@ On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine
 
 On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
 
-On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."))))
+On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+
+    (test-equal? "Recites first verse"
+      (twelve-days 1 1)
+      "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.")
+
+    (test-equal? "Recites fifth through seventh verses"
+      (twelve-days 5 7)
+      "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+
+On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+
+    ))
+
+  (run-tests suite))
