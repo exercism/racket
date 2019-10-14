11fb434e38b6ff6a0073a05a70d816baceb1e119
Tim Meehan <btmeehan@gmail.com>
2019-10-13 19:50:46 -0700

    Updated "raindrops" to canonical-data.json v1.1.0

--- exercises/raindrops/raindrops-test.rkt
+++ exercises/raindrops/raindrops-test.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 
+; Tests adapted from `problem-specifications/canonical-data.json v1.1.0
 (require "raindrops.rkt")
 
 (module+ test
@@ -9,20 +10,76 @@
     (test-suite
      "raindrops tests"
 
-     (test-equal? "test-1" (convert 1) "1")
-     (test-equal? "test-3" (convert 3) "Pling")
-     (test-equal? "test-5" (convert 5) "Plang")
-     (test-equal? "test-7" (convert 7) "Plong")
-     (test-equal? "test-6" (convert 6) "Pling")
-     (test-equal? "test-9" (convert 9) "Pling")
-     (test-equal? "test-10" (convert 10) "Plang")
-     (test-equal? "test-15" (convert 15) "PlingPlang")
-     (test-equal? "test-21" (convert 21) "PlingPlong")
-     (test-equal? "test-25" (convert 25) "Plang")
-     (test-equal? "test-35" (convert 35) "PlangPlong")
-     (test-equal? "test-49" (convert 49) "Plong")
-     (test-equal? "test-52" (convert 52) "52")
-     (test-equal? "test-105" (convert 105) "PlingPlangPlong")
-     (test-equal? "test-12121" (convert 12121) "12121")))
+     (test-equal? "the sound for 1 is 1"
+                  (convert 1)
+                  "1")
+
+     (test-equal? "the sound for 3 is Pling"
+                  (convert 3)
+                  "Pling")
+
+     (test-equal? "the sound for 5 is Plang"
+                  (convert 5)
+                  "Plang")
+
+     (test-equal? "the sound for 7 is Plong"
+                  (convert 7)
+                  "Plong")
+
+     (test-equal? "the sound for 6 is Pling as it has a factor of 3"
+                  (convert 6)
+                  "Pling")
+
+     (test-equal? "2 to the power 3 does not make a raindrop sound as 3 is the exponent not the base"
+                  (convert 8)
+                  "8")
+
+     (test-equal? "the sound for 9 is Pling as it has a factor 3"
+                  (convert 9)
+                  "Pling")
+
+     (test-equal? "the sound for 10 is Plang as it has a factor 5"
+                  (convert 10)
+                  "Plang")
+
+     (test-equal? "the sound for 14 is Plong as it has a factor of 7"
+                  (convert 14)
+                  "Plong")
+
+     (test-equal? "the sound for 15 is PlingPlang as it has factors 3 and 5"
+                  (convert 15)
+                  "PlingPlang")
+
+     (test-equal? "the sound for 21 is PlingPlong as it has factors 3 and 7"
+                  (convert 21)
+                  "PlingPlong")
+
+     (test-equal? "the sound for 25 is Plang as it has a factor 5"
+                  (convert 25)
+                  "Plang")
+
+     (test-equal? "the sound for 27 is Pling as it has a factor 3"
+                  (convert 27)
+                  "Pling")
+
+     (test-equal? "the sound for 35 is PlangPlong as it has factors 5 and 7"
+                  (convert 35)
+                  "PlangPlong")
+
+     (test-equal? "the sound for 49 is Plong as it has a factor 7"
+                  (convert 49)
+                  "Plong")
+
+     (test-equal? "the sound for 52 is 52"
+                  (convert 52)
+                  "52")
+
+     (test-equal? "the sound for 105 is PlingPlangPlong as it has factors 3, 5 and 7"
+                  (convert 105)
+                  "PlingPlangPlong")
+
+     (test-equal? "the sound for 3125 is Plang as it has a factor 5"
+                  (convert 3125)
+                  "Plang")))
 
   (run-tests suite))
