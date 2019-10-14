1bca1db487d93d7a495dd03c681ea5fb42b9415f
Tim Meehan <btmeehan@gmail.com>
2019-10-13 18:56:40 -0700

    ; Updated "roman-numerals" to canonical-data.json v1.2.0

--- exercises/roman-numerals/roman-numerals-test.rkt
+++ exercises/roman-numerals/roman-numerals-test.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+; Tests adapted from `problem-specifications/canonical-data.json v1.2.0
 (require "roman-numerals.rkt")
 
 (module+ test
@@ -9,75 +10,79 @@
     (test-suite
      "Roman Numeral tests"
 
-     (test-equal? "1"
+     (test-equal? "1 is a single I"
                   (to-roman 1)
                   "I")
 
-     (test-equal? "2"
+     (test-equal? "2 is two I's"
                   (to-roman 2)
                   "II")
 
-     (test-equal? "3"
+     (test-equal? "3 is three I's"
                   (to-roman 3)
                   "III")
 
-     (test-equal? "4"
+     (test-equal? "4, being 5 - 1, is IV"
                   (to-roman 4)
                   "IV")
 
-     (test-equal? "5"
+     (test-equal? "5 is a single V"
                   (to-roman 5)
                   "V")
 
-     (test-equal? "6"
+     (test-equal? "6, being 5 + 1, is VI"
                   (to-roman 6)
                   "VI")
 
-     (test-equal? "9"
+     (test-equal? "9, being 10 - 1, is IX"
                   (to-roman 9)
                   "IX")
 
-     (test-equal? "27"
-                  (to-roman 27)
-                  "XXVII")
+     (test-equal? "20 is two X's"
+                  (to-roman 20)
+                  "XX")
 
-     (test-equal? "48"
+     (test-equal? "48 is not 50 - 2 but rather 40 + 8"
                   (to-roman 48)
                   "XLVIII")
 
-     (test-equal? "59"
+     (test-equal? "49 is not 40 + 5 + 4 but rather 50 - 10 + 1"
+                  (to-roman 49)
+                  "XLIX")
+
+     (test-equal? "50 is a single L"
                   (to-roman 59)
                   "LIX")
 
-     (test-equal? "93"
+     (test-equal? "90, being 100 - 10, is XC"
                   (to-roman 93)
                   "XCIII")
 
-     (test-equal? "141"
+     (test-equal? "100 is a single C"
                   (to-roman 141)
                   "CXLI")
 
-     (test-equal? "163"
+     (test-equal? "60, being 50 + 10, is LX"
                   (to-roman 163)
                   "CLXIII")
 
-     (test-equal? "402"
+     (test-equal? "400, being 500 - 100, is CD"
                   (to-roman 402)
                   "CDII")
 
-     (test-equal? "575"
+     (test-equal? "500 is a single D"
                   (to-roman 575)
                   "DLXXV")
 
-     (test-equal? "911"
+     (test-equal? "900, being 1000 - 100, is CM"
                   (to-roman 911)
                   "CMXI")
 
-     (test-equal? "1024"
+     (test-equal? "1000 is a single M"
                   (to-roman 1024)
                   "MXXIV")
 
-     (test-equal? "3000"
+     (test-equal? "3000 is three M's"
                   (to-roman 3000)
                   "MMM")
      ))
