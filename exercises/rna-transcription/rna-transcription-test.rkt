3de912a4ba6e79edcb134165b59555c421bc6b26
Tim Meehan <btmeehan@gmail.com>
2019-10-13 19:28:30 -0700

    Updated "rna-transcription" to canonical-data.json v1.3.0

--- exercises/rna-transcription/rna-transcription-test.rkt
+++ exercises/rna-transcription/rna-transcription-test.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 
+; Tests adapted from `problem-specifications/canonical-data.json v1.3.0
 (require "rna-transcription.rkt")
 
 (module+ test
@@ -8,12 +9,28 @@
   (define suite
     (test-suite
      "RNA transcription tests"
+     (test-equal? "Empty RNA sequence"
+                  (to-rna "")
+                  "")
 
-     (test-equal? "transcribes cytosine to guanine" (to-rna "C") "G")
-     (test-equal? "transcribes guanine to cytosine" (to-rna "G") "C")
-     (test-equal? "transcribes adenine to uracil" (to-rna "A") "U")
-     (test-equal? "transcribes thymine to adenine" (to-rna "T") "A")
-     (test-equal? "transcribes all nucleotides" (to-rna "ACGTGGTCTTAA") "UGCACCAGAAUU")
-     (test-exn "it validates dna strands" exn:fail? (lambda () (to-rna "XCGFGGTDTTAA")))))
+     (test-equal? "RNA compliment of cytosine is guanine"
+                  (to-rna "C")
+                  "G")
+
+     (test-equal? "RNA compliment of guanine is cytosine"
+                  (to-rna "G")
+                  "C")
+
+     (test-equal? "RNA compliment of thymine is adenine"
+                  (to-rna "T")
+                  "A")
+
+     (test-equal? "RNA compliment of adenine is uracil"
+                  (to-rna "A")
+                  "U")
+
+     (test-equal? "RNA compliment"
+                  (to-rna "ACGTGGTCTTAA")
+                  "UGCACCAGAAUU")))
 
   (run-tests suite))
