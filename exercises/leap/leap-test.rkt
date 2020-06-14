91c677b22737fd67360fbad6d2cf358531a76179
Son Dang <sondangdev@gmail.com>
2020-06-15 00:25:22 +0700

    Fix typos in leap exercise

--- exercises/leap/leap-test.rkt
+++ exercises/leap/leap-test.rkt
@@ -10,9 +10,9 @@
      "leap tests"
 
      (test-eqv? "vanilla-leap-year" (leap-year? 1996) #t)
-     (test-eqv? "any-old-year" (leap-year? 2015)#f)
+     (test-eqv? "any-odd-year" (leap-year? 2015) #f)
      (test-eqv? "non-leap-even-year" (leap-year? 1998) #f)
-     (test-eqv? "century" (leap-year? 2100)#f)
+     (test-eqv? "century" (leap-year? 2100) #f)
      (test-eqv? "not an exceptional-century" (leap-year? 1800) #f)
      (test-eqv? "exceptional-century" (leap-year? 2000) #t)))
 
