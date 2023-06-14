ca99f6eba674db9bea4c7e4c76f6a45a33871859
Andras Nagy <andras@farwestern.com>
2023-06-14 11:32:13 -0700

    Initial implementation

---
+++ exercises/practice/resistor-color-duo/.meta/example.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(define resistor-colors
+  '#(black brown red orange yellow green blue violet grey white))
+
+(define (color-code colors)
+  (match colors
+    [(list first second _ ...)
+     (let* ([tens (vector-member first resistor-colors)]
+            [ones (vector-member second resistor-colors)]
+            [value (+ (* tens 10) ones)])
+       value)]
+    [ _ (error "Invalid colors argument")]))
