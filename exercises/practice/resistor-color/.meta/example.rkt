b8ab8d45261fc27c6259ade11c88f2bb81fa9229
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-09-07 05:54:10 -0700

    Add resistor-color exercise (#272)

---
+++ exercises/practice/resistor-color/.meta/example.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+(provide color-code colors)
+
+(define resistor-colors
+  (vector "black" "brown" "red" "orange" "yellow" "green" "blue" "violet" "grey" "white"))
+
+(define (color-code color)
+  (vector-member color resistor-colors))
+
+(define (colors)
+  (vector->list resistor-colors))
