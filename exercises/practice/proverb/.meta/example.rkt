971b6477667e655e6334f867b82cd73eea9981a0
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-10-30 14:20:17 +0100

    Use for/list instead of helper function

--- exercises/practice/proverb/.meta/example.rkt
+++ exercises/practice/proverb/.meta/example.rkt
@@ -5,12 +5,8 @@
 (define (recite lst)
   (cond
     [(empty? lst) '()]
-    [else (define (verse index)
-            (cond
-              [(= index (sub1 (length lst))) (list (format "And all for the want of a ~a." (list-ref lst 0)))]
-              [else (append
-                     (list (format "For want of a ~a the ~a was lost."
-                                   (list-ref lst index)
-                                   (list-ref lst (add1 index))))
-                     (verse (add1 index)))]))
-          (verse 0)]))
+    [else (append
+           (for/list ([value lst]
+                      [next-value (rest lst)])
+             (format "For want of a ~a the ~a was lost." value next-value))
+           (list (format "And all for the want of a ~a." (first lst))))]))
