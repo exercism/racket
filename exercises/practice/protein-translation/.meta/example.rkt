4e8cee3ee770e9487262d3ca7412006a0cd23798
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-10-30 16:46:06 +0100

    Add protein-translation example

--- exercises/practice/protein-translation/.meta/example.rkt
+++ exercises/practice/protein-translation/.meta/example.rkt
@@ -2,5 +2,23 @@
 
 (provide proteins)
 
+(define (translate protein)
+  (case protein
+    [("AUG") "Methionine"]
+    [("UUU" "UUC") "Phenylalanine"]
+    [("UUA" "UUG") "Leucine"]
+    [("UCU" "UCC" "UCA" "UCG") "Serine"]
+    [("UAU" "UAC") "Tyrosine"]
+    [("UGU" "UGC") "Cysteine"]
+    [("UGG") "Tryptophan"]
+    [("UAA" "UAG" "UGA") '()]
+    [else (error "Invalid codon")]))
+
 (define (proteins strand)
-  (error "Not implemented yet"))
+  (cond
+    [(zero? (string-length strand)) '()]
+    [(< (string-length strand) 3) (error "Incomplete RNA sequence can't translate")]
+    [else (define codon (translate (substring strand 0 3)))
+          (cond
+            [(empty? codon) '()]
+            [else (cons codon (proteins (substring strand 3)))])]))
