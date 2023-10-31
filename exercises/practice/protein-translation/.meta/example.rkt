efc371f3e4859a2a40b0841296ae2f73b49b588f
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-10-31 10:35:57 +0100

    Move define out of cond for clarity

--- exercises/practice/protein-translation/.meta/example.rkt
+++ exercises/practice/protein-translation/.meta/example.rkt
@@ -17,10 +17,9 @@
     [else (error "Invalid codon")]))
 
 (define (proteins strand)
+  (define (codon) (translate (substring strand 0 3)))
   (cond
     [(zero? (string-length strand)) '()]
     [(< (string-length strand) 3) (error "Incomplete RNA sequence can't translate")]
-    [else (define codon (translate (substring strand 0 3)))
-          (cond
-            [(empty-string? codon) '()]
-            [else (cons codon (proteins (substring strand 3)))])]))
+    [else (cond [(empty-string? (codon)) '()]
+                [else (cons (codon) (proteins (substring strand 3)))])]))
