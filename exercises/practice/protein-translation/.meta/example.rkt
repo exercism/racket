198dc6c45ba9642a022371675067546fa71e27ec
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-10-31 10:25:33 +0100

    Replace empty list by empty string for consistency

--- exercises/practice/protein-translation/.meta/example.rkt
+++ exercises/practice/protein-translation/.meta/example.rkt
@@ -2,6 +2,8 @@
 
 (provide proteins)
 
+(define (empty-string? str) ((negate non-empty-string?) str))
+
 (define (translate protein)
   (case protein
     [("AUG") "Methionine"]
@@ -11,7 +13,7 @@
     [("UAU" "UAC") "Tyrosine"]
     [("UGU" "UGC") "Cysteine"]
     [("UGG") "Tryptophan"]
-    [("UAA" "UAG" "UGA") '()]
+    [("UAA" "UAG" "UGA") ""]
     [else (error "Invalid codon")]))
 
 (define (proteins strand)
@@ -20,5 +22,5 @@
     [(< (string-length strand) 3) (error "Incomplete RNA sequence can't translate")]
     [else (define codon (translate (substring strand 0 3)))
           (cond
-            [(empty? codon) '()]
+            [(empty-string? codon) '()]
             [else (cons codon (proteins (substring strand 3)))])]))
