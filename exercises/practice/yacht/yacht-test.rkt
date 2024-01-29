105c1fda74e8f6123a378b6422cc22e77e2605b3
Blake Lewis <blake.h.lewis@gmail.com>
2024-01-28 18:20:17 -0800

    Capitalize category names in test descriptions

--- exercises/practice/yacht/yacht-test.rkt
+++ exercises/practice/yacht/yacht-test.rkt
@@ -22,7 +22,7 @@
       (test-equal? "Ones, out of order"
 		   (yacht '(3 1 1 5 1) "ones") 3)
 
-      (test-equal? "No ones"
+      (test-equal? "No Ones"
 		   (yacht '(4 3 6 5 5) "ones") 0)
 
       (test-equal? "Twos"
@@ -31,10 +31,10 @@
       (test-equal? "Fours"
 		   (yacht '(1 4 1 4 1) "fours") 8)
 
-      (test-equal? "Yacht counted as threes"
+      (test-equal? "Yacht counted as Threes"
 		   (yacht '(3 3 3 3 3) "threes") 15)
 
-      (test-equal? "Yacht of 3s counted as fives"
+      (test-equal? "Yacht of 3s counted as Fives"
 		   (yacht '(3 3 3 3 3) "fives") 0)
 
       (test-equal? "Fives"
@@ -49,13 +49,13 @@
       (test-equal? "Full house three small, two big"
 		   (yacht '(5 3 3 5 3) "full house") 19)
 
-      (test-equal? "Two pair is not a full house"
+      (test-equal? "Two pair is not a Full house"
 		   (yacht '(2 2 4 4 5) "full house") 0)
 
-      (test-equal? "Four of a kind is not a full house"
+      (test-equal? "Four of a kind is not a Full house"
 		   (yacht '(1 4 4 4 4) "full house") 0)
 
-      (test-equal? "Yacht is not a full house"
+      (test-equal? "Yacht is not a Full house"
 		   (yacht '(2 2 2 2 2) "full house") 0)
 
       (test-equal? "Four of a Kind"
@@ -73,28 +73,28 @@
       (test-equal? "Little Straight as Big Straight"
 		   (yacht '(1 2 3 4 5) "big straight") 0)
 
-      (test-equal? "Four in order but not a little straight"
+      (test-equal? "Four in order but not a Little Straight"
 		   (yacht '(1 1 2 3 4) "little straight") 0)
 
-      (test-equal? "No pairs but not a little straight"
+      (test-equal? "No pairs but not a Little Straight"
 		   (yacht '(1 2 3 4 6) "little straight") 0)
 
-      (test-equal? "Minimum is 1, maximum is 5, but not a little straight"
+      (test-equal? "Minimum is 1, maximum is 5, but not a Little Straight"
 		   (yacht '(1 1 3 4 5) "little straight") 0)
 
       (test-equal? "Big Straight"
 		   (yacht '(4 6 2 5 3) "big straight") 30)
 
-      (test-equal? "Big Straight as little straight"
+      (test-equal? "Big Straight as Little Straight"
 		   (yacht '(6 5 4 3 2) "little straight") 0)
 
-      (test-equal? "No pairs but not a big straight"
+      (test-equal? "No pairs but not a Big Straight"
 		   (yacht '(6 5 4 3 1) "big straight") 0)
 
       (test-equal? "Choice"
 		   (yacht '(3 3 5 6 6) "choice") 23)
 
-      (test-equal? "Yacht as choice"
+      (test-equal? "Yacht as Choice"
 		   (yacht '(2 2 2 2 2) "choice") 10)))
 
   (run-tests suite))
