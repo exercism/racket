c374e4a7aed0520dda639e69014122073cf5b87a
Blake Lewis <blake.h.lewis@gmail.com>
2024-02-02 18:33:12 -0800

    Use test-true and test-false instead of test-equal?

--- exercises/practice/luhn/luhn-test.rkt
+++ exercises/practice/luhn/luhn-test.rkt
@@ -10,71 +10,63 @@
     (test-suite
       "Luhn tests"
 
-      (test-equal? "single digit strings can not be valid"
-                   (valid? "1") #f)
+      (test-false "single digit strings can not be valid" (valid? "1"))
 
-      (test-equal? "a single zero is invalid"
-                   (valid? "0") #f)
+      (test-false "a single zero is invalid" (valid? "0"))
 
-      (test-equal? "a simple valid SIN that remains valid if reversed"
-                   (valid? "059") #t)
+      (test-true "a simple valid SIN that remains valid if reversed"
+                  (valid? "059"))
 
-      (test-equal? "a simple valid SIN that becomes invalid if reversed"
-                   (valid? "59") #t)
+      (test-true "a simple valid SIN that becomes invalid if reversed"
+                  (valid? "59"))
 
-      (test-equal? "a valid Canadian SIN"
-                   (valid? "055 444 285") #t)
+      (test-true "a valid Canadian SIN" (valid? "055 444 285"))
 
-      (test-equal? "invalid Canadian SIN"
-                   (valid? "055 444 286") #f)
+      (test-false "invalid Canadian SIN" (valid? "055 444 286"))
 
-      (test-equal? "invalid credit card"
-                   (valid? "8273 1232 7352 0569") #f)
+      (test-false "invalid credit card" (valid? "8273 1232 7352 0569"))
 
-      (test-equal? "invalid long number with an even remainder"
-                   (valid? "1 2345 6789 1234 5678 9012") #f)
+      (test-false "invalid long number with an even remainder"
+                   (valid? "1 2345 6789 1234 5678 9012"))
 
-      (test-equal? "invalid long number with a remainder divisible by 5"
-                   (valid? "1 2345 6789 1234 5678 9013") #f)
+      (test-false "invalid long number with a remainder divisible by 5"
+                   (valid? "1 2345 6789 1234 5678 9013"))
 
-      (test-equal? "valid number with an even number of digits"
-                   (valid? "095 245 88") #t)
+      (test-true "valid number with an even number of digits"
+                  (valid? "095 245 88"))
 
-      (test-equal? "valid number with an odd number of spaces"
-                   (valid? "234 567 891 234") #t)
+      (test-true "valid number with an odd number of spaces"
+                  (valid? "234 567 891 234"))
 
-      (test-equal? "valid strings with a non-digit added at the end become invalid"
-                   (valid? "059a") #f)
+      (test-false "valid strings with a non-digit added at the end become invalid"
+                   (valid? "059a"))
 
-      (test-equal? "valid strings with punctuation included become invalid"
-                   (valid? "055-444-285") #f)
+      (test-false "valid strings with punctuation included become invalid"
+                  (valid? "055-444-285"))
 
-      (test-equal? "valid strings with symbols included become invalid"
-                   (valid? "055# 444$ 285") #f)
+      (test-false "valid strings with symbols included become invalid"
+                  (valid? "055# 444$ 285"))
 
-      (test-equal? "single zero with space is invalid"
-                   (valid? " 0") #f)
+      (test-false "single zero with space is invalid" (valid? " 0"))
 
-      (test-equal? "more than a single zero is valid"
-                   (valid? "0000 0") #t)
+      (test-true "more than a single zero is valid" (valid? "0000 0"))
 
-      (test-equal? "input digit 9 is correctly converted to output digit 9"
-                   (valid? "091") #t)
+      (test-true "input digit 9 is correctly converted to output digit 9"
+                 (valid? "091"))
 
-      (test-equal? "very long input is valid"
-                   (valid? "9999999999 9999999999 9999999999 9999999999") #t)
+      (test-true "very long input is valid"
+                 (valid? "9999999999 9999999999 9999999999 9999999999"))
 
-      (test-equal? "valid luhn with an odd number of digits and non zero first digit"
-                   (valid? "109") #t)
+      (test-true "valid luhn with an odd number of digits and non zero first digit"
+                 (valid? "109"))
 
-      (test-equal? "using ascii value for non-doubled non-digit isn't allowed"
-                   (valid? "055b 444 285") #f)
+      (test-false "using ascii value for non-doubled non-digit isn't allowed"
+                  (valid? "055b 444 285"))
 
-      (test-equal? "using ascii value for doubled non-digit isn't allowed"
-                   (valid? ":9") #f)
+      (test-false "using ascii value for doubled non-digit isn't allowed"
+                  (valid? ":9"))
 
-      (test-equal? "non-numeric, non-space char in the middle with a sum that's divisible by 10 isn't allowed"
-                   (valid? "59%59") #f)))
+      (test-false "non-numeric, non-space char in the middle with a sum that's divisible by 10 isn't allowed"
+                  (valid? "59%59"))))
   
   (run-tests suite))
-
